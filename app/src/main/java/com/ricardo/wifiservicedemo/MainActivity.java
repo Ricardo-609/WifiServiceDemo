12b809bb3965f45c77ad17ad8c00565e165d89a1
Ricardo-609 <673298654@qq.com>
2023-11-30 09:29:07 +0800

    temp1

--- app/src/main/java/com/ricardo/wifiservicedemo/MainActivity.java
+++ app/src/main/java/com/ricardo/wifiservicedemo/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private final String TAG = "[MainActivity]";
     private IWifiService mService;
 
+//    private Handler mHandler;
+    
     private ServiceConnection mServiceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
@@ -43,6 +45,26 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, WifiService.class);
         bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+
+//        startSubThread();
     }
+
+//    private void startSubThread() {
+//        mHandler = new Handler(Looper.getMainLooper());
+//        mHandler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                try {
+//                    if (mService != null) mService.openSta();
+//                } catch (RemoteException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                // 每隔300毫秒再次调用自身，实现定时任务
+//                mHandler.postDelayed(this, 300);
+//            }
+//        }, 0);
+//    }
 
 }
